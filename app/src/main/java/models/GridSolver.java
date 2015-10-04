e52dfffc894b12e5ba81c3cf40799b497b304f21
Pushkar Lanka <pushkar.lanka@gmail.com>
2015-10-04 02:37:57 -0500

    initial commit

---
+++ app/src/main/java/models/GridSolver.java
@@ -0,0 +1,106 @@
+package models;
+
+/**
+ * Created by pushkar on 9/26/15.
+ */
+public class GridSolver {
+
+    private int[][] grid;
+    private boolean solvable;
+    private int dimension;
+    private int miniGridDimension;
+
+    public GridSolver(int[][] grid) {
+        this.grid = getDeepCopy(grid);
+        this.dimension = grid.length;
+        this.miniGridDimension = (int) Math.sqrt(dimension);
+    }
+
+    public boolean isSolvable() {
+        return solvable;
+    }
+
+    public int[][] getSolution() {
+        solvable = solverHelper();
+        return grid;
+    }
+
+    private boolean solverHelper() {
+        int[] zeroPosition = new int[2];
+        boolean noZerosLeft = !findNextZero(zeroPosition);
+
+        if(noZerosLeft) {
+            return true;
+        }
+
+        int row = zeroPosition[0];
+        int col = zeroPosition[1];
+
+        for(int num = 1; num <= 9; num++) {
+            grid[row][col] = num;
+            if(gridIsValid(row, col)) {
+                if(solverHelper())
+                    return true;
+            }
+        }
+        grid[row][col] = 0;
+        return false;
+    }
+
+    private boolean findNextZero(int[] zeroPosition) {
+        for(int r = 0; r < dimension; r++) {
+            for(int c = 0; c < dimension; c++) {
+                if(grid[r][c] == 0) {
+                    zeroPosition[0] = r;
+                    zeroPosition[1] = c;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean gridIsValid(int row, int col) {
+        return rowColValid(row, col) && miniGridValid(row, col);
+    }
+
+    private boolean rowColValid(int row, int col) {
+        int value = grid[row][col];
+
+        for(int c = 0; c < dimension; c++) {
+            if(c != col && grid[row][c] == value)
+                return false;
+        }
+        for(int r = 0; r < dimension; r++) {
+            if(r != row && grid[r][col] == value)
+                return false;
+        }
+        return true;
+    }
+
+    private boolean miniGridValid(int row, int col) {
+        int value = grid[row][col];
+
+        int startRow = (row / miniGridDimension) * miniGridDimension;
+        int startCol = (col / miniGridDimension) * miniGridDimension;
+
+        for(int r = startRow; r < startRow + miniGridDimension; r++) {
+            for(int c = startCol; c < startCol + miniGridDimension; c++) {
+                if(!(r == row && c == col) && grid[r][c] == value)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    private int[][] getDeepCopy(int[][] inputGrid) {
+        int[][] deepCopy = new int[inputGrid.length][inputGrid[0].length];
+
+        for(int r = 0; r < inputGrid.length; r++) {
+            for(int c = 0; c < inputGrid[0].length; c++) {
+                deepCopy[r][c] = inputGrid[r][c];
+            }
+        }
+        return deepCopy;
+    }
+}
---
+++ app/src/main/java/pushkarlanka/gosudoku/GridActivity.java
@@ -0,0 +1,112 @@
+package pushkarlanka.gosudoku;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+import models.GridSolver;
+
+public class GridActivity extends Activity {
+
+    private static final int DIMENSION = 9;
+    private EditText[][] gridItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid);
+
+        gridItems = new EditText[DIMENSION][DIMENSION];
+
+        String initialGridValues = "400000060000097000007200000005601470001008090000520000010800000006000030030902740";
+        setInitialGrid(initialGridValues);
+
+        setOnClickListeners();
+    }
+
+    private void setInitialGrid(String initialGridValues) {
+
+        TableLayout grid = (TableLayout) findViewById(R.id.main_grid);
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        for(int i = 0; i < DIMENSION; i++) {
+            TableRow gridRow = (TableRow) inflater.inflate(R.layout.grid_row,null);
+            for(int j = 0; j < DIMENSION; j++) {
+                EditText gridItem = (EditText) inflater.inflate(R.layout.grid_item, null);
+                String gridItemValue = String.valueOf(initialGridValues.charAt(i * DIMENSION + j));
+                gridItem.setText(gridItemValue);
+
+                if(!gridItemValue.equals("0")) {
+                    gridItem.setTextColor(Color.parseColor("#000000"));
+                    gridItem.setEnabled(false);
+                }
+
+                gridItems[i][j] = gridItem;
+                gridRow.addView(gridItem);
+            }
+            grid.addView(gridRow);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_grid, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setOnClickListeners() {
+
+        final GridSolver gridSolver = new GridSolver(getCurrentGridValues());
+        final int[][] solvedGrid = gridSolver.getSolution();
+
+        Button solveBtn = (Button) findViewById(R.id.solve_btn);
+        solveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (gridSolver.isSolvable()) {
+                    for (int i = 0; i < DIMENSION; i++) {
+                        for (int j = 0; j < DIMENSION; j++) {
+                            gridItems[i][j].setText(String.valueOf(solvedGrid[i][j]));
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    private int[][] getCurrentGridValues() {
+        int[][] currentGridValues = new int[DIMENSION][DIMENSION];
+        for(int i = 0; i < DIMENSION; i++) {
+            for(int j = 0; j < DIMENSION; j++) {
+                currentGridValues[i][j] = Integer.parseInt(gridItems[i][j].getText().toString());
+            }
+        }
+        return currentGridValues;
+    }
+}
